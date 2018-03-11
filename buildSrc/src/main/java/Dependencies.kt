a81ac899ec7bbc4efe49a4eee0c144923347a6cd
M. Reza Nasirloo <M.Reza.Nasirloo@gmai.com>
2018-03-12 00:16:54 +0330

    Cleans up all gradle.build files

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,6 +8,7 @@ object Versions {
     val vSdkCompile = 26
     val vVersionCode = 1
     val vVersionName = "1.0"
+    val vBuildTool = "26.0.3"
 
     val vSupportLib = "26.1.0"
     val vRxjava2 = "2.1.10"
@@ -15,14 +16,34 @@ object Versions {
 }
 
 object Deps {
-    val depSupportLibrary = "com.android.support:appcompat-v7:${Versions.vSupportLib}"
-    val depRxjava2 = "io.reactivex.rxjava2:rxjava:${Versions.vRxjava2}"
+    val depSupportAppCompat = "com.android.support:appcompat-v7:${Versions.vSupportLib}"
+    val depSupportFragment = "com.android.support:support-fragment:${Versions.vSupportLib}"
+    val depSupportAnnotation = "com.android.support:support-annotations:${Versions.vSupportLib}"
+    val depSupportDesign = "com.android.support:design:${Versions.vSupportLib}"
+    val depSupportCardView = "com.android.support:cardview-v7:${Versions.vSupportLib}"
+    val depConstraintLayout = "com.android.support.constraint:constraint-layout:1.0.2"
+
+    val depConductor = "com.bluelinelabs:conductor:2.1.3"
+    val depSparkButton = "com.github.varunest:sparkbutton:1.0.5"
+
     val depRxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.vRxAndroid}"
+    val depRxjava2 = "io.reactivex.rxjava2:rxjava:${Versions.vRxjava2}"
     val depRxRelays = "com.jakewharton.rxrelay2:rxrelay:2.0.0"
+
+    val depDaggerCompiler = "com.google.dagger:dagger-compiler:2.10"
+    val depDagger = "com.google.dagger:dagger:2.10"
+
+
     val depJavax = "javax.inject:javax.inject:1"
+    val depJavaPoet = "com.squareup:javapoet:1.8.0"
+    val depAutoCommon = "com.google.auto:auto-common:0.8"
+    val depAutoService = "com.google.auto.service:auto-service:1.0-rc2"
 
     val depTestJunit = "junit:junit:4.12"
-    val depTestRunner = "com.android.support.test:runner:0.5"
-    val depTestEspressoCore = "com.android.support.test.espresso:espresso-core:2.2.2"
+    val depTestTruth = "com.google.truth:truth:0.35"
     val depTestMokito = "org.mockito:mockito-core:2.7.22"
+    val depTestRunner = "com.android.support.test:runner:0.5"
+    val depTestMokitoAndroid = "org.mockito:mockito-android:2.7.22"
+    val depTestCompileTesting = "com.google.testing.compile:compile-testing:0.10"
+    val depTestEspressoCore = "com.android.support.test.espresso:espresso-core:2.2.2"
 }
